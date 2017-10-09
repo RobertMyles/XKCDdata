fc835e8c3ec9c6ffd0ccc9b38ebbddac1b107125
Colin <colin@thinkr.fr>
2017-10-09 10:51:18 +0200

    updates

--- R/getData.R
+++ R/getData.R
@@ -1,28 +1,30 @@
-#' @importFrom httr GET
-#' @importFrom httr content
+#' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
-#' @importFrom tibble as_tibble
+#' @importFrom glue glue
+#' @importFrom dplyr mutate
+#' @importFrom assertthat assert_that
 #' @title Get XKCD Comic Data
 #' @description Download data from individual XKCD comics from
 #' <https://xkcd.com/>.
 #' @param comic \code{integer}. Comic number.
+#' @return a tibble with comic data
 #' @examples \dontrun{get_comic(comic = 614)}
 #' @export
 get_comic <- function(comic = NULL){
+  assertthat::assert_that(is.numeric(comic), msg = "comic should be numeric")
 
   if(is.null(comic)){
     url <- "http://xkcd.com/info.0.json"
   } else{
-    url <- paste0("http://xkcd.com/", comic, "/info.0.json")
+    url <- glue::glue("http://xkcd.com/{comic}/info.0.json")
   }
+
   df <- httr::GET(url)
 
-  if(df$status_code != 200){
-    stop("Comic not found.")
-  }
+  assertthat::assert_that(df$status_code == 200, msg = "Comic not found.")
 
-  df <- httr::content(df, as = "text")
-  df2 <- jsonlite::fromJSON(df)
-  df3 <- tibble::as_tibble(df2)
-  return(df3)
+  httr::content(df, as = "text") %>%
+    jsonlite::fromJSON() %>%
+    dplyr::as_tibble() %>%
+    dplyr::mutate(link = glue::glue("http://xkcd.com/{comic}"))
 }
